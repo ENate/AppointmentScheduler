d15615e7afe2ba80d7408cd0deec9c9e080a2854
ENate <nathaniel@aims.ac.za>
2024-03-25 12:21:53 +0100

    Added customizer to ignore APIs

--- src/main/java/com/example/slabiak/appointmentscheduler/security/SecurityConfig.java
+++ src/main/java/com/example/slabiak/appointmentscheduler/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,9 +31,25 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(
                 authorize -> authorize
                 .requestMatchers("/actuator/").permitAll()
+                // .requestMatchers("/").permitAll()
+                .requestMatchers("/").hasAnyRole("CUSTOMER", "PROVIDER", "ADMIN")
+                .requestMatchers("/api/**").hasAnyRole("CUSTOMER", "PROVIDER", "ADMIN")
+                .requestMatchers("/customers/all").hasRole("ADMIN")
+                .requestMatchers("/providers/new").hasRole("ADMIN")
+                .requestMatchers("/invoices/all").hasRole("ADMIN")
+                .requestMatchers("/providers/all").hasRole("ADMIN")
+                .requestMatchers("/customers/**").hasAnyRole("CUSTOMER", "ADMIN")
+                .requestMatchers("/providers/availability/**").hasRole("PROVIDER")
+                .requestMatchers("/providers/**").hasAnyRole("PROVIDER", "ADMIN")
+                .requestMatchers("/works/**").hasRole("ADMIN")
+                .requestMatchers("/exchange/**").hasRole("CUSTOMER")
+                .requestMatchers("/appointments/new/**").hasRole("CUSTOMER")
+                .requestMatchers("/appointments/**").hasAnyRole("CUSTOMER", "PROVIDER", "ADMIN")
+                .requestMatchers("/invoices/**").hasAnyRole("CUSTOMER", "PROVIDER", "ADMIN")
                 .anyRequest().authenticated())
                 .formLogin(form -> form
                     .loginPage("/login")
@@ -45,12 +62,22 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
         auth.setUserDetailsService(customUserDetailsService);
         auth.setPasswordEncoder(passwordEncoder);
         return auth;
+    }
+
+    // WARNING: If you are configuring WebSecurity to ignore requests, 
+    // consider using permitAll
+
+    @Bean
+    public WebSecurityCustomizer  webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/customers/new/**");
     }
 
 }
